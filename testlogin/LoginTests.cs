36d5bbab616d48e2def77d093cfe836deb633d15
Omartmx <omarpk148@gmail.com>
2025-11-06 00:37:49 -0500

    cambio jenkins

--- testlogin/LoginTests.cs
+++ testlogin/LoginTests.cs
@@ -15,7 +15,7 @@ namespace BlazorLoginApp.Tests // Asegúrate que coincida con tu namespace
             var component = RenderComponent<Login>();
     
             component.Find("input#email").Change("admin@test.com");
-            component.Find("input#password").Change("12345");
+            component.Find("input#password").Change("123456");
             component.Find("button").Click();
 
             Assert.Contains("¡Login exitoso!", component.Markup);
